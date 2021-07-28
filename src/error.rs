6b09537cae55e12e1d4e0606a5ffdc2e4e9cb4a0
DerFetzer <kontakt@der-fetzer.de>
2021-07-28 23:32:31 +0200

    Add indicator for DW1000 error type

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,7 @@ use defmt::Format;
 #[derive(Debug, Clone, Copy, PartialEq, Format)]
 pub enum Error {
     Interface,
-    DW1000,
+    DW1000(u8),
     InvalidState,
     WouldBlock,
 }
@@ -13,7 +13,20 @@ impl From<dw1000::Error<DwSpiType, DwCsType>> for Error {
     fn from(e: dw1000::Error<DwSpiType, DwCsType>) -> Self {
         match e {
             dw1000::Error::Spi(_) => Error::Interface,
-            _ => Error::DW1000,
+            dw1000::Error::Fcs => Error::DW1000(0),
+            dw1000::Error::Phy => Error::DW1000(1),
+            dw1000::Error::BufferTooSmall { .. } => Error::DW1000(2),
+            dw1000::Error::ReedSolomon => Error::DW1000(3),
+            dw1000::Error::FrameWaitTimeout => Error::DW1000(4),
+            dw1000::Error::Overrun => Error::DW1000(5),
+            dw1000::Error::PreambleDetectionTimeout => Error::DW1000(6),
+            dw1000::Error::SfdTimeout => Error::DW1000(7),
+            dw1000::Error::FrameFilteringRejection => Error::DW1000(8),
+            dw1000::Error::Frame(_) => Error::DW1000(9),
+            dw1000::Error::DelayedSendTooLate => Error::DW1000(10),
+            dw1000::Error::DelayedSendPowerUpWarning => Error::DW1000(11),
+            dw1000::Error::Ssmarshal(_) => Error::DW1000(12),
+            dw1000::Error::InvalidConfiguration => Error::DW1000(13),
         }
     }
 }
